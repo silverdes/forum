67cc3e2d6607cd8d063a3a01b259d108fc6bffcb
silverdev <ouaiji04@gmail.com>
2020-06-21 18:59:53 +0100

    Add dummy Data , show it in index page with pagination

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TopicController@index')->name('topics.index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('topics', 'TopicController')->except(['index']);
